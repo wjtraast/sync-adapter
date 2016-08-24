6a8688cee58d455bd77543327b2278af8cff3b44
Gerben Vis <gerben.vis@gmail.com>
2016-08-24 08:11:57 +0200

    added restTemplate for OnlyOnce API

--- src/main/java/nl/onlyonce/adapter/service/OnlyOnceApiServiceImpl.java
+++ src/main/java/nl/onlyonce/adapter/service/OnlyOnceApiServiceImpl.java
@@ -3,7 +3,10 @@ package nl.onlyonce.adapter.service;
 import nl.onlyonce.adapter.model.Activity;
 import nl.onlyonce.adapter.model.type.AdapterType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.Resource;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -13,10 +16,13 @@ import java.util.List;
 public class OnlyOnceApiServiceImpl implements OnlyOnceApiService {
 
 
+    @Resource
+    RestTemplate restTemplate;
+
+
     @Override
     public List<Activity> getActivities(final AdapterType type) {
-
-        return null;
-
+        Activity[] activityArray = restTemplate.getForObject("http://demo.onlyonce.nl/api/", Activity[].class);
+        return Arrays.asList(activityArray);
     }
 }
