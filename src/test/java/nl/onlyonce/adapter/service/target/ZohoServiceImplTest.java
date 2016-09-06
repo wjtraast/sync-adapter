906b83be51c5c6015a392f854b0fae22dff73ac2
Gerben Vis <gerben.vis@gmail.com>
2016-09-06 09:16:56 +0200

    updated zoho mappings

--- src/test/java/nl/onlyonce/adapter/service/target/ZohoServiceImplTest.java
+++ src/test/java/nl/onlyonce/adapter/service/target/ZohoServiceImplTest.java
@@ -23,15 +23,11 @@ public class ZohoServiceImplTest {
 
         Date SYNC_DATE = new Date();
         SimpleDateFormat dateFullFormatter = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
-
         String syncDateString = dateFullFormatter.format(SYNC_DATE);
-
-
         String EXPECTED_VALUE = "<Contacts><row no=\"1\"><FL val=\"Salutation\">Male</FL><FL val=\"First Name\">Jan</FL><FL val=\"Last Name\">Jansen</FL><FL val=\"Date of Birth\">20/02/1972</FL><FL val=\"Mailing Street\">Kerkstraat</FL><FL val=\"Title\">Ing. JJ</FL><FL val=\"Description\">Last synced by Only Once at " + syncDateString  + "\nCard name: My Card</FL></row></Contacts>";
         service = new ZohoServiceImpl();
 
         ZohoRequestMessage message = Fixtures.getFullZohoRequstMessage();
-
         ZohoContact contact = service.tranform(message, SYNC_DATE);
         String zohoContactXml = contact.toString();
         System.out.println(zohoContactXml);
