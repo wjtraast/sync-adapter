8d4485ca710e70ee49e57e77465aab24f1a36679
Gerben Vis <gerben.vis@gmail.com>
2016-09-08 09:17:25 +0200

    updates

---
+++ src/test/java/nl/onlyonce/adapter/service/target/TestUtils.java
@@ -0,0 +1,11 @@
+package nl.onlyonce.adapter.service.target;
+
+/**
+ * @author: Gerben
+ */
+public class TestUtils {
+
+    public static String addFLElement(String data, String value) {
+        return data  + value;
+    }
+}
--- src/test/java/nl/onlyonce/adapter/service/target/ZohoServiceImplTest.java
+++ src/test/java/nl/onlyonce/adapter/service/target/ZohoServiceImplTest.java
@@ -24,7 +24,18 @@ public class ZohoServiceImplTest {
         Date SyncDate = new Date();
         SimpleDateFormat dateFullFormatter = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
         String syncDateString = dateFullFormatter.format(SyncDate);
-        String EXPECTED_VALUE = "<Contacts><row no=\"1\"><FL val=\"Salutation\">Male</FL><FL val=\"First Name\">Jan</FL><FL val=\"Last Name\">Jansen</FL><FL val=\"Date of Birth\">20/02/1972</FL><FL val=\"Mailing Street\">Kerkstraat</FL><FL val=\"Title\">Ing. JJ</FL><FL val=\"Description\">Last synced by Only Once at " + syncDateString  + "\nCard name: My Card</FL></row></Contacts>";
+
+        String data = "<Contacts><row no=\"1\">";
+        data = TestUtils.addFLElement(data, "<FL val=\"Salutation\">Male</FL>");
+        data = TestUtils.addFLElement(data, "<FL val=\"First Name\">Jan</FL>");
+        data = TestUtils.addFLElement(data, "<FL val=\"Last Name\">Jansen</FL>");
+        data = TestUtils.addFLElement(data, "<FL val=\"Date of Birth\">20/02/1972</FL>");
+        data = TestUtils.addFLElement(data, "<FL val=\"Mailing Street\">Kerkstraat</FL>");
+        data = TestUtils.addFLElement(data, "<FL val=\"Title\">Ing. JJ</FL>");
+        data = TestUtils.addFLElement(data, "<FL val=\"Description\">Last synced by Only Once at " + syncDateString + "\nCard name: My Card</FL>");
+        data = data + "</row></Contacts>";
+
+        String EXPECTED_VALUE = data;
         service = new ZohoServiceImpl();
 
         ZohoRequestMessage message = Fixtures.getContactZohoRequestMessage();
