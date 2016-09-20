3b5c7d4687873906f54d31e9dfbacf94fac0202d
Gerben Vis <gerben.vis@gmail.com>
2016-09-20 10:23:55 +0200

    update

--- src/test/java/nl/onlyonce/adapter/service/target/CarerixApiServiceImplTest.java
+++ src/test/java/nl/onlyonce/adapter/service/target/CarerixApiServiceImplTest.java
@@ -1,6 +1,7 @@
 package nl.onlyonce.adapter.service.target;
 
 import nl.onlyonce.adapter.service.api.CarerixApiService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -14,14 +15,14 @@ public class CarerixApiServiceImplTest {
 
     private CarerixApiService service;
 
-//
-//    @Test
-//    public void testSearch() {
+
+    @Test
+    public void testSearch() {
 //
 //        service = new CarerixApiServiceImpl();
 //
 //        String employeeId = service.findEmployee("12124124");
 //        Assert.assertEquals("55905", employeeId);
-//    }
+    }
 
 }
